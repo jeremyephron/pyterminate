d075df8abd68e8b683e7ee7742cde3652ae00fc1
Jeremy Ephron <jeremye@cs.stanford.edu>
2022-04-03 23:49:33 -0700

    [docs] Update README and docstrings

--- pyterminate/__init__.py
+++ pyterminate/__init__.py
@@ -1,14 +1,24 @@
+"""
+Module: pyterminate
+-------------------
+Defines decorators for registering and unregistering functions to be called
+at program termination.
+
+"""
+
 from collections import defaultdict
 import atexit
 import signal
 import sys
 from types import FrameType
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import Any, Callable, DefaultDict, Dict, List, Optional, Set, Tuple
 
 
-_registered_funcs = set()
-_func_to_wrapper = {}
-_signal_to_prev_handler = defaultdict(lambda: defaultdict(list))
+_registered_funcs: Set[Callable] = set()
+_func_to_wrapper: Dict[Callable, Callable] = {}
+_signal_to_prev_handler: DefaultDict[Callable, DefaultDict[int, List[Callable]]] = (
+    defaultdict(lambda: defaultdict(list))
+)
 
 
 def register(
@@ -16,17 +26,36 @@ def register(
     *,
     args: tuple = tuple(),
     kwargs: Optional[Dict[str, Any]] = None,
-    signals=(signal.SIGTERM,),
+    signals: Tuple[int] = (signal.SIGTERM,),
     successful_exit: bool = False,
     keyboard_interrupt_on_sigint: bool = False,
 ) -> Callable:
-    kwargs = kwargs or {}
+    """
+    Registers a function to be called at program termination or creates a
+    decorator to do so.
+
+    Args:
+        func: Function to register.
+        args: Positional arguments to pass to the function when called.
+        kwargs: Keyword arguments to pass to the function when called.
+        signals: Signals to call the given function for when handled.
+        successful_exit: Whether to always exit with a zero exit code.
+            Otherwise, exits with the code of the signal.
+        keyboard_interrupt_on_sigint: Whether to raise a KeyboardInterrupt on
+            SIGINT. Otherwise, exits with code SIGINT.
+
+    Returns:
+        func: The given function if not None.
+        decorator: A decorator that will apply the given settings to the
+            decorated function.
+
+    """
 
     def decorator(func: Callable) -> Callable:
         return _register_impl(
             func,
             args,
-            kwargs,
+            kwargs or {},
             signals,
             successful_exit,
             keyboard_interrupt_on_sigint
@@ -36,6 +65,15 @@ def register(
 
 
 def unregister(func: Callable) -> None:
+    """
+    Unregisters a previously registered function from being called at exit.
+
+    Args:
+        func: A previously registered function. The call is a no-op if not
+            previously registered.
+
+    """
+
     if func in _func_to_wrapper:
         atexit.unregister(_func_to_wrapper[func])
 
@@ -50,6 +88,33 @@ def _register_impl(
     successful_exit: bool,
     keyboard_interrupt_on_sigint: bool,
 ) -> Callable:
+    """
+    Registers a function to be called at program termination.
+
+    This function is the internal implementation of registration, and should
+    not be called by a user, who should called register() instead.
+
+    Idempotent handlers are created for both atexit and signal handling.
+    The currently handled signal is ignored during the signal handler to allow
+    for the registered functions to complete when potentially receiving
+    multiple repeated signals. However, it can be canceled upon receipt of
+    another signal.
+
+    Args:
+        func: Function to register.
+        args: Positional arguments to pass to the function when called.
+        kwargs: Keyword arguments to pass to the function when called.
+        signals: Signals to call the given function for when handled.
+        successful_exit: Whether to always exit with a zero exit code.
+            Otherwise, exits with the code of the signal.
+        keyboard_interrupt_on_sigint: Whether to raise a KeyboardInterrupt on
+            SIGINT. Otherwise, exits with code SIGINT.
+
+    Returns:
+        func: The given function.
+
+    """
+
     def exit_handler(*args: Any, **kwargs: Any) -> Any:
         if func not in _registered_funcs:
             return
@@ -57,7 +122,7 @@ def _register_impl(
         _registered_funcs.remove(func)
         return func(*args, **kwargs)
 
-    def signal_handler(sig: int, frame: FrameType):
+    def signal_handler(sig: int, frame: Optional[FrameType]) -> None:
         signal.signal(sig, signal.SIG_IGN)
 
         exit_handler(*args, **kwargs)
